2e8db8d75b98f2aa3714faaba696b5eb6873b9fa
Sean-Stilwell <sstil051@uottawa.ca>
2024-06-03 13:13:59 -0400

    Updating

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,6 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
+EXPOSE 80
+
 CMD ["python", "./app.py"]
--- app.py
+++ app.py
@@ -218,4 +218,4 @@ def get_celestial_body(n_clicks, name):
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0', port=8080)
+    app.run_server(debug=True, host='0.0.0.0', port=80)
